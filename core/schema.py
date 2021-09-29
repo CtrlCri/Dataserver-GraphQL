461c4a37e268a1835c43253ca4ca7a82c7609fca
Cristian Army <armycrih@gmail.com>
2021-09-29 20:02:33 -0300

    first mutations

--- core/schema.py
+++ core/schema.py
@@ -6,5 +6,7 @@ import datasets.schema
 class Query(datasets.schema.Query, graphene.ObjectType):
     pass
 
+class Mutation(datasets.schema.Mutation, graphene.ObjectType):
+    pass
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- datasets/schema.py
+++ datasets/schema.py
@@ -14,3 +14,30 @@ class Query(graphene.ObjectType):
 
     def resolve_datasets(self, info, **kwargs):
         return Dataset.objects.all()
+
+#1
+class CreateDataset(graphene.Mutation):
+    id = graphene.Int()
+    title = graphene.String()
+    about = graphene.String()
+
+    #2
+    class Arguments:
+        title = graphene.String()
+        about = graphene.String()
+
+    #3
+    def mutate(self, info, title, about):
+        dataset = Dataset(title=title, about=about)
+        dataset.save()
+
+        return CreateDataset(
+            id = dataset.id,
+            title = dataset.title,
+            about = dataset.about,
+        )
+
+
+#4
+class Mutation(graphene.ObjectType):
+    create_dataset = CreateDataset.Field()
